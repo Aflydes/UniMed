c9dc908a5fa993d03b274940aa32b81375ccd0f6
Aflydes <aflydes@gmail.com>
2022-03-27 00:45:37 +0300

    fis adaptiv and add new page

--- js/functions.js
+++ js/functions.js
@@ -10,4 +10,30 @@
                 document.querySelector('body').classList.toggle('_scroll-lock');
                 document.querySelector('.header__contacts').classList.toggle('active');
             }
-        }
+}
+
+ /*  Yandex map init  */
+ ymaps.ready(createMap);
+ function createMap(){
+     var myMap = new ymaps.Map("map", {
+         center: [44.951593, 34.104082],
+         zoom: 15
+     }),
+     customPlacemark = new ymaps.Placemark([44.951593, 34.104082], 
+         {
+             iconLayout: 'default#image',
+             iconImageHref: 'img/map-marker.png',
+             iconImageSize: [42, 63],
+             iconImageOffset: [-5, -38]
+         }
+     );
+     myMap.geoObjects.add(customPlacemark);
+}
+
+$('.consultation__list-item').hover( function(){
+    let hoverItem = $(this).attr('data-hover');
+    if( hoverItem != ''){
+        $('.' + hoverItem).toggleClass('hidden');
+        $('.' + hoverItem).toggleClass('visible');
+    }
+});
